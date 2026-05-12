0d7dc0f2211590d415a488084cb5860e650587a4
BishowDevkota <bishow8848@gmail.com>
2026-05-12 22:13:11 +0545

    trying to fix error

--- store/useCart.ts
+++ store/useCart.ts
@@ -17,7 +17,7 @@ export const useCart = create<CartState>((set, get) => ({
   setItems: (items) => set({ items }),
   add: (productId, quantity = 1) => {
     const items = get().items.slice();
-    const idx = items.findIndex((i) => i.productId === productId);
+    const idx = items.findIndex((i) => i.productId === productId); 
     if (idx > -1) items[idx].quantity = quantity;
     else items.push({ productId, quantity });
     set({ items });
